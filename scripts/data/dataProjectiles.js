24c1eaea22f98c74d93b8fb6eecb6129060dcf08
Thickant <141985730+Thickant@users.noreply.github.com>
2024-09-22 20:25:53 -0400

    Update dataProjectiles.js

--- scripts/data/dataProjectiles.js
+++ scripts/data/dataProjectiles.js
@@ -7,7 +7,7 @@ define([''], function () {
   var projectiles = {
     arrow: {
       anim: 'arrow',
-      startPos: [0, 8],
+      startPos: [-8, 2],
       movement: 'horizontal',
       dir: 'AIM'
     },
